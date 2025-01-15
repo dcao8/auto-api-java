4478beea7dc477e5161c45b455ec3812da9bd900
dcao8 <daych93@gmail.com>
2025-01-15 23:15:32 +0700

    [ID-10] remove duplicate test case

--- src/test/java/data/country/GetCountryData.java
+++ src/test/java/data/country/GetCountryData.java
@@ -42,6 +42,56 @@ public class GetCountryData {
             ]
             """;
 
+    public static final String GET_ALL_COUNTRIES_WITH_GDP = """
+            [
+                {
+                         "name": "Viet Nam",
+                         "code": "VN",
+                         "gdp": 223.9
+                     },
+                     {
+                         "name": "USA",
+                         "code": "US",
+                         "gdp": 21427.5
+                     },
+                     {
+                         "name": "Canada",
+                         "code": "CA",
+                         "gdp": 1930
+                     },
+                     {
+                         "name": "UK",
+                         "code": "GB",
+                         "gdp": 2827
+                     },
+                     {
+                         "name": "France",
+                         "code": "FR",
+                         "gdp": 2718
+                     },
+                     {
+                         "name": "Japan",
+                         "code": "JP",
+                         "gdp": 5081
+                     },
+                     {
+                         "name": "India",
+                         "code": "IN",
+                         "gdp": 2875
+                     },
+                     {
+                         "name": "China",
+                         "code": "CN",
+                         "gdp": 14342.9
+                     },
+                     {
+                         "name": "Brazil",
+                         "code": "BR",
+                         "gdp": 1868
+                     }
+            ]
+            """;
+
     public static final String GET_ALL_COUNTRIES_WITH_PRIVATE = """
             [
                 {
--- src/test/java/tests/country/GetCountryTests.java
+++ src/test/java/tests/country/GetCountryTests.java
@@ -20,8 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static data.country.GetCountryData.GET_ALL_COUNTRIES;
-import static data.country.GetCountryData.GET_ALL_COUNTRIES_WITH_PRIVATE;
+import static data.country.GetCountryData.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -56,6 +55,29 @@ public class GetCountryTests {
         assertThat(response.asString(), jsonEquals(GET_ALL_COUNTRIES).when(Option.IGNORING_ARRAY_ORDER));
     }
 
+    @Test
+    void verifyGetCountryWithGdpApiSchema() {
+        RestAssured.given().log().all()
+                .get("/api/v2/countries")
+                .then().log().all()
+                .statusCode(200)
+                .assertThat().body(matchesJsonSchemaInClasspath("data/get-country/get-country-with-gdp-json-schema.json"));
+    }
+
+    @Test
+    void verifyGetCountryWithGdpApiResponseCorrectData() {
+        Response response = RestAssured.given().log().all()
+                .get("/api/v2/countries");
+        //1. Verify Status code
+        assertThat(response.statusCode(), equalTo(200));
+        //2. Verify Header if needs
+        assertThat(response.header("Content-Type"), equalTo("application/json; charset=utf-8"));
+        assertThat(response.header("X-Powered-By"), equalTo("Express"));
+        //3. Verify Body
+        assertThat(response.asString(), jsonEquals(GET_ALL_COUNTRIES_WITH_GDP).when(Option.IGNORING_ARRAY_ORDER));
+        System.out.println(response.asString());
+    }
+
     static Stream<Map<String, String>> countryProvider() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         List<Map<String, String>> data = mapper.readValue(GET_ALL_COUNTRIES, new TypeReference<List<Map<String, String>>>() {
@@ -65,7 +87,7 @@ public class GetCountryTests {
 
     @ParameterizedTest
     @MethodSource("countryProvider")
-    void verifyGetCountriesApiResponseCorrectData(Map<String, String> country) {
+    void verifyGetCountryByCodeApiResponseCorrectData(Map<String, String> country) {
         Response response = RestAssured.given().log().all()
                 .get("/api/v1/countries/{code}", country.get("code"));
         //1. Verify Status code
