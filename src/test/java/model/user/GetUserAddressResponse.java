11caed9dcc21500892cda1c9d720e7537e3b422b
dcao8 <daych93@gmail.com>
2025-01-16 16:15:23 +0700

    [ID-12] Handle token timeout, verify user was storaged in system by GET API

---
+++ src/test/java/model/user/GetUserAddressResponse.java
@@ -0,0 +1,23 @@
+package model.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetUserAddressResponse {
+    private String id;
+    private String customerId;
+    private String streetNumber;
+    private String street;
+    private String ward;
+    private String district;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+    private String createdAt;
+    private String updatedAt;
+}
--- src/test/java/tests/user/CreateUserTests.java
+++ src/test/java/tests/user/CreateUserTests.java
@@ -5,44 +5,117 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import model.login.LoginRequest;
 import model.login.LoginResponse;
-import model.user.CreateUserResponse;
-import model.user.UserRequest;
+import model.user.*;
 import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.LoginUtils;
 import utils.RestAssuredUtils;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+
 public class CreateUserTests {
+
+    static String token;
+    static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    static final String HEADER_AUTHORIZATION = "Authorization";
+    static final String HEADER_CONTENT_TYPE = "Content-Type";
+    static final String HEADER_POWER_BY = "X-Powered-By";
+    static final String CONTENT_TYPE = "application/json; charset=utf-8";
+    static final String POWER_BY = "Express";
+    static final String EMAIL_TEMPLATE = "api_%s@api.com";
+    static final String CREATE_USER_API = "/api/user";
+    static final String GET_USER_API = "/api/user/%s";
+
     @BeforeAll
     static void setUp() {
         RestAssuredUtils.setUp();
     }
 
+    @BeforeEach
+    void beforeEach() {
+        token = LoginUtils.getToken();
+    }
+
     @Test
     void verifyCreateUserSuccessful() {
-        LoginRequest loginRequest = LoginRequest.getDefault();
-        LoginResponse loginResponse = RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(loginRequest)
-                .post("/api/login")
-                .as(LoginResponse.class);
-        String token = String.format("Bearer %s", loginResponse.getToken());
         long randomNumber = System.currentTimeMillis();
-        String randomEmail = String.format("api_%s@api.com", randomNumber);
+        String randomEmail = String.format(EMAIL_TEMPLATE, randomNumber);
         UserRequest userRequest = UserRequest.getDefault();
         userRequest.setEmail(randomEmail);
+        LocalDateTime timeBeforeCreateUser = LocalDateTime.now(ZoneId.of("Z"));
         Response response = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .header("Authorization", token)
+                .header(HEADER_AUTHORIZATION, token)
                 .body(userRequest)
-                .post("/api/user");
+                .post(CREATE_USER_API);
         SoftAssertions softAssertions = new SoftAssertions();
         //1. Verify Status code
         softAssertions.assertThat(response.statusCode()).isEqualTo(200);
         //2. Verify Header if needs
-        softAssertions.assertThat(response.header("Content-Type")).isEqualTo("application/json; charset=utf-8");
-        softAssertions.assertThat(response.header("X-Powered-By")).isEqualTo("Express");
+        softAssertions.assertThat(response.header(HEADER_CONTENT_TYPE)).isEqualTo(CONTENT_TYPE);
+        softAssertions.assertThat(response.header(HEADER_POWER_BY)).isEqualTo(POWER_BY);
+        //3. Verify Body
+        CreateUserResponse createUserResponse = response.as(CreateUserResponse.class);
+        softAssertions.assertThat(StringUtils.isNoneBlank(createUserResponse.getId())).isTrue();
+        softAssertions.assertThat(createUserResponse.getMessage()).isEqualTo("Customer created");
+        //4. Double check that user has been stored in system
+        Response getResponse = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .header(HEADER_AUTHORIZATION, token)
+                .get(String.format(GET_USER_API, createUserResponse.getId()));
+        softAssertions.assertThat(getResponse.statusCode()).isEqualTo(200);
+        softAssertions.assertAll();
+        GetUserResponse getUserResponse = getResponse.as(GetUserResponse.class);
+        assertThatJson(getUserResponse).whenIgnoringPaths("$..id", "$..createdAt", "$..updatedAt", "$..customerId")
+                .isEqualTo(userRequest);
+        softAssertions = new SoftAssertions();
+        softAssertions.assertThat(getUserResponse.getId()).isEqualTo(createUserResponse.getId());
+        LocalDateTime timeAfterCreateUser = LocalDateTime.now(ZoneId.of("Z"));
+        for (GetUserAddressResponse getUserAddressResponse : getUserResponse.getAddresses()) {
+            softAssertions.assertThat(getUserAddressResponse.getCustomerId()).isEqualTo(createUserResponse.getId());
+            verifyDateTime(softAssertions, getUserAddressResponse.getCreatedAt(), timeBeforeCreateUser, timeAfterCreateUser);
+            verifyDateTime(softAssertions, getUserAddressResponse.getUpdatedAt(), timeBeforeCreateUser, timeAfterCreateUser);
+        }
+        verifyDateTime(softAssertions, getUserResponse.getCreatedAt(), timeBeforeCreateUser, timeAfterCreateUser);
+        verifyDateTime(softAssertions, getUserResponse.getUpdatedAt(), timeBeforeCreateUser, timeAfterCreateUser);
+        softAssertions.assertAll();
+    }
+
+    void verifyDateTime(SoftAssertions softAssertions, String targetDateTime, LocalDateTime timeBefore, LocalDateTime timeAfter) {
+        LocalDateTime localDateTime = LocalDateTime.parse(targetDateTime, DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
+        softAssertions.assertThat(localDateTime.isAfter(timeBefore)).isTrue();
+        softAssertions.assertThat(localDateTime.isBefore(timeAfter)).isTrue();
+    }
+
+    @Test
+    void verifyCreateUserSuccessfulWithTwoAddress() {
+        long randomNumber = System.currentTimeMillis();
+        String randomEmail = String.format(EMAIL_TEMPLATE, randomNumber);
+        UserRequest userRequest = UserRequest.getDefault();
+        UserAddressRequest userAddressRequest1 = UserAddressRequest.getDefault();
+        UserAddressRequest userAddressRequest2 = UserAddressRequest.getDefault();
+        userAddressRequest2.setStreetNumber("456");
+        userRequest.setAddresses(List.of(userAddressRequest1, userAddressRequest2));
+        userRequest.setEmail(randomEmail);
+        Response response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .header(HEADER_AUTHORIZATION, token)
+                .body(userRequest)
+                .post(CREATE_USER_API);
+        SoftAssertions softAssertions = new SoftAssertions();
+        //1. Verify Status code
+        softAssertions.assertThat(response.statusCode()).isEqualTo(200);
+        //2. Verify Header if needs
+        softAssertions.assertThat(response.header(HEADER_CONTENT_TYPE)).isEqualTo(CONTENT_TYPE);
+        softAssertions.assertThat(response.header(HEADER_POWER_BY)).isEqualTo(POWER_BY);
         //3. Verify Body
         CreateUserResponse createUserResponse = response.as(CreateUserResponse.class);
         softAssertions.assertThat(StringUtils.isNoneBlank(createUserResponse.getId())).isTrue();
